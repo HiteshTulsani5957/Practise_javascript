a7256d6cb050180b02d433ae321d9b10358a0d44
HiteshTulsani5957 <htulsani53@gmail.com>
2026-01-10 12:57:32 +0530

    Function commit

---
+++ Day1/Functions.js
@@ -0,0 +1,36 @@
+// function OnePlusAvg (x,y){
+//     return 1+(x+y)/2
+// }
+// let a = 2;
+// let b = 3;
+// let c = 4;
+
+// console.log("Avg of a+b is",OnePlusAvg (a,b))
+// console.log("Avg of b+c is",OnePlusAvg (b,a))
+// console.log("Avg of c+a is",OnePlusAvg (c,a))
+
+
+// const sum=(p,q)=>{
+// return p+q
+// }
+// console.log(sum(4,2)) 
+
+
+// const hello =()=>{
+//     console.log("hey hello")
+// }
+// hello();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
