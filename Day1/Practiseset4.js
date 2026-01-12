e92f2a7eff922c73ceafe84e077c041579f142fd
HiteshTulsani5957 <htulsani53@gmail.com>
2026-01-12 13:13:57 +0530

    Practiseset4 commit

---
+++ Day1/Practiseset4.js
@@ -0,0 +1,9 @@
+// Problem no 1
+// let a= "amit"
+// console.log(a.toUpperCase())
+
+// Problem no 2
+// let b= "Please give 1000"
+// let c= Number.parseInt (b.slice("Please give".length))
+// console.log(c)
+// console.log(typeof c)
