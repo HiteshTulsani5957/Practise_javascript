ce85c9faeacbde6673d792e7e5ef72bcd506eaa6
HiteshTulsani5957 <htulsani53@gmail.com>
2026-01-12 11:46:59 +0530

    Practiseset1 commit

---
+++ Day1/Practiseset3.js
@@ -0,0 +1,37 @@
+// // Problem no 1
+// let no = {
+//     amit: 21,
+//     gmit: 22,
+//     ymit: 23,
+//     pmit: 24,
+// }
+// for(let i=0; i<Object.keys(no).length; i++){
+// console.log("The marks of" + Object.keys(no)[i] + no[Object.keys(no)[i]]);
+// }
+
+
+// Problem no 2
+// let no = {
+//     amit: 21,
+//     gmit: 22,
+//     ymit: 23,
+//     pmit: 24,
+// }
+// for(let i in no){
+// console.log("The marks of" + i + no[i]);
+// }
+
+// // Problem no 3
+// let cn =54
+// let i
+// while(i != cn){
+//     console.log("Incorrect");
+//      i = prompt('Enter number');
+// }
+// console.log("correct")
+
+// Problem no 4
+// const mean = (a,b,c,d)=> {
+//     return(a + b + c+ d)
+// }
+// console.log(mean(3,4,5,6)+"done");
