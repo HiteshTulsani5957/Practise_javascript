d9acbaf49bcadcbb02a088f653512edfa29076c9
HiteshTulsani5957 <htulsani53@gmail.com>
2026-01-16 15:17:57 +0530

    Chapter 5 - Practice Set on Arrays commit

---
+++ Day1/Chapter5-Practice Set.js
@@ -0,0 +1,40 @@
+//Practice1
+// let a = [3,4,5,67,8,9]
+// let b = prompt("ENTER NO")
+// b= Number.parseInt(b)
+// a.push(b)
+// console.log(a)
+
+//Practice2
+// let a = [2,3,4,5,6,7,30,50,70]
+// let b;
+// do{
+
+//  b = prompt("ENTER NO")
+// b=Number.parseInt(b)
+// a.push(b)
+// }while(b!=0);
+//     console.log(a)
+
+
+//Practice3
+//  let a = [2,3,4,5,6,7,30,50,70]
+//  let b = a.filter((x)=>{
+//     return x%10==0
+//  })
+//  console.log(b)
+
+
+//Practice4
+//  let a = [2,3,4,5,6,7,30,50,70]
+//  let b = a.map((x)=>{
+//     return x*x
+//  })
+//  console.log(b)
+
+//Practice5
+//  let a = [1,2,3,4]
+//  let b = a.reduce((x1,x2)=>{
+//     return x1*x2
+//  })
+//  console.log(b)
