6db38c7c6f3e95b7f65da017057d7a1e0067ea77
HiteshTulsani5957 <htulsani53@gmail.com>
2026-01-18 10:22:09 +0530

    Chapter 6 - Practice Set commit

---
+++ Day1/Day2/index.js
@@ -0,0 +1,70 @@
+
+// //PROBLEM NO 1
+// let a= prompt("ENTER THE AGE")
+// a= Number.parseInt(a)
+// const candrive =(a)=>{
+//     return a>=18?true:false
+// }
+// if(candrive(a)){
+//     alert("YOU CAN DRIVE")
+// }
+// else{
+//         alert("YOU CAN't DRIVE")
+// }
+
+
+//PROBLEM NO 2
+// let = runagain = true
+
+// const candrive = (a) => {
+//     return a >= 18 ? true : false
+// }
+
+// while (runagain) {
+//     let a = prompt("ENTER THE AGE")
+//     a = Number.parseInt(a)
+//     if (candrive(a)) {
+//         alert("YOU CAN DRIVE")
+//     }
+//     else {
+//         alert("YOU CAN't DRIVE")
+//     }
+//     runagain = confirm("DO YOU WANT TO PLAY AGAIN")
+// }
+
+
+//PROBLEM NO 3
+// let = runagain = true
+
+// const candrive = (a) => {
+//     return a >= 18 ? true : false
+// }
+
+// while (runagain) {
+//     let a = prompt("ENTER THE AGE")
+//     a = Number.parseInt(a)
+//     if (a < 0) {
+//         console.error("negative age")
+//         break;
+//     }
+//     if (candrive(a)) {
+//         alert("YOU CAN DRIVE")
+//     }
+//     else {
+//         alert("YOU CAN't DRIVE")
+//     }
+//     runagain = confirm("DO YOU WANT TO PLAY AGAIN")
+// }
+
+
+//PROBLEM NO 4
+// let number = prompt("ENTER THE number")
+// number = Number.parseInt(number)
+// if (number > 4) {
+//     location.href = "https://github.com/"
+// }
+
+
+//PROBLEM NO 5
+// let colour = prompt("ENTER THE BACKGROUND COLOUR")
+// document.body.style.background=(colour)
--- Day1/index.js
+++ Day1/index.js
@@ -1,21 +1,23 @@
-alert("ENTER THE VALUE OF A")
+// alert("ENTER THE VALUE OF A")
 
-let a = prompt("enter a here")
-a = Number.parseInt(a)
-document.write(a)
-alert(
-    typeof a
-)
+// let a = prompt("enter a here")
+// a = Number.parseInt(a)
+// document.write(a)
+// alert(
+//     typeof a
+// )
 
-let write = confirm("write here")
-if(write){
-document.write("write")
+// let write = confirm("write here")
+// if(write){
+// document.write("write")
 
-}
-else{
-    document.write("not write")
-}
+// }
+// else{
+//     document.write("not write")
+// }
 
 
 
-document.body.style.background="red"
+// document.body.style.background="red"
+
+
