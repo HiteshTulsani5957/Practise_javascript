d570107cce0b0411d83b077789ccd40e23d63bb8
HiteshTulsani5957 <htulsani53@gmail.com>
2026-01-22 10:54:40 +0530

    Element only Navigation commit

---
+++ Day1/Day2/day3/index.js
@@ -0,0 +1,3 @@
+let a=document.body
+console.log( "the first child is" , a.firstChild)
+console.log("the first Element child is" , a.firstElementChild)//this will show the first element not the text one
