5a77ae2fb7d7df3311d3f65978d4e5f233db57bc
HiteshTulsani5957 <htulsani53@gmail.com>
2026-01-22 11:33:00 +0530

    Table Navigation commit

---
+++ day4/index.js
@@ -0,0 +1,13 @@
+let t =document.body.firstElementChild.firstElementChild
+console.log(t)
+console.log(t.rows)
+console.log(t.captions)
+console.log(t.thead)
+console.log(t.tfoot)
+console.log(t.tr)
+console.log(t)
+console.log(t)
+console.log(t)
+console.log(t)
+console.log(t)
+console.log(t)
