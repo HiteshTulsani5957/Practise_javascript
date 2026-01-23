1511063231772c39406a142759743ad3eab7791d
HiteshTulsani5957 <htulsani53@gmail.com>
2026-01-23 14:09:51 +0530

    Searching the DOM commit

---
+++ day4/day5/index.js
@@ -0,0 +1,23 @@
+// let c = document.getElementsByClassName("card-title")[0]
+// c.style.color="red"
+
+
+// let c = document.getElementById("first")//gives only one element
+// c.style.color="puple"
+
+// let c = document.querySelectorAll(".card-title")//gives all element
+// c[0].style.color="red"
+// c[1].style.color="blue"
+// c[2].style.color="red"
+//  console.log(c)
+
+// document.querySelector(".this").style.color="red"
+// document.querySelector(".this").style.background="red"
+// document.querySelector(".this").style.color="red"
+
+// console.log(document.getElementsByTagName("a"))//gives all element
+// console.log(document.body.getElementsByTagName("a"))
+// console.log(document.querySelector(".card").getElementsByTagName("a"))
+
+
+// console.log(document.getElementsByName("search"))
