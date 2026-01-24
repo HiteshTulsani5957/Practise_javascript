484837839539be09f3cd248392cbd515104c7685
HiteshTulsani5957 <htulsani53@gmail.com>
2026-01-24 19:18:30 +0530

    Exercise no 7 commit

---
+++ day4/day5/day6/index.js
@@ -0,0 +1,12 @@
+ //PROBLEM NO 1
+//  document.getElementsByTagName("nav")[0].firstElementChild.style.color="red"
+
+ //PROBLEM NO 2
+//  document.getElementsByTagName("nav")[0].firstElementChild.style.color="red"
+//  document.getElementsByTagName("nav")[0].lastElementChild.style.color="blue"
+
+
+//   //PROBLEM NO 3
+//  Array.from(document.getElementsByTagName("li")).forEach((Element)=>{
+//     Element.style.background="cyan"
+//  })
