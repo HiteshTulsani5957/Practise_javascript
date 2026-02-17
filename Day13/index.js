14b42276486e53f909836f26d1e454f2f97ecfee
HiteshTulsani5957 <htulsani53@gmail.com>
2026-02-17 09:50:46 +0530

    Error Handling: try and catch commit

---
+++ Day13/index.js
@@ -0,0 +1,19 @@
+setTimeout(()=>{
+  console.log("first");
+},1000)
+
+try{
+console.log(hello);
+}
+
+catch(err){
+    console.log("hello ok");
+}
+
+setTimeout(()=>{
+  console.log("second");
+},2000)
+
+setTimeout(()=>{
+  console.log("third");
+},3000)
